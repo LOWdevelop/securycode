ab9c0607c1af93f577a67aee89f594fda2fd7df8
lowDevelop <lowdevlop@gmail.com>
2023-04-30 07:47:53 -0300

    First Commit

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from .secury_lib import *
---
+++ secury_lib.py
@@ -0,0 +1,10 @@
+
+
+#  ->   IMPORTS .
+import requests
+
+
+#  ->  Get you acess , from firebase
+class get_credentials:
+    def __init__(self,token):
+        self.token = token
